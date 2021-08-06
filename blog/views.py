c48882d5a9c78c1b4129294b4ef333b8d63f8888
Prajwal <prajwal.un16@gmail.com>
2021-08-06 19:16:52 +0530

    Added dummy data using context

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+
+posts = [
+	{
+
+		'author' : "Prajwal",
+		'title' : 'BLog Post 1',
+		'content' : 'First post content',
+		'date_posted' : 'August 6th 2021'
+
+	},
+	{
+	'author' : "Prajwal UN",
+		'title' : 'BLog Post 2',
+		'content' : 'Second post content',
+		'date_posted' : 'August 7th 2021'
+	}
+
+
+
+
+
+]
 
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'posts':posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'blog/about.html', {'title':'_About_'})
+
 
