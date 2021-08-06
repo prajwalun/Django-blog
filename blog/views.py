4df53168f7c36ec13e12e0c6807d7605186b0c0a
Prajwal <prajwal.un16@gmail.com>
2021-08-06 23:43:08 +0530

    Connected the database/Added admin page functionalities

--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 
 
-posts = [
-	{
-
-		'author' : "Prajwal",
-		'title' : 'BLog Post 1',
-		'content' : 'First post content',
-		'date_posted' : 'August 6th 2021'
-
-	},
-	{
-	'author' : "Prajwal UN",
-		'title' : 'BLog Post 2',
-		'content' : 'Second post content',
-		'date_posted' : 'August 7th 2021'
-	}
-
-
-
-
-
-]
 
 def home(request):
 	context = {
-		'posts':posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
